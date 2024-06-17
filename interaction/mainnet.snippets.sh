a574a0489c5f2f511eb13ecce79bef58dca7322d
Mihai Daniel Eremia <22687907+mihaieremia@users.noreply.github.com>
2024-06-18 02:40:28 +0400

    deploy mainnet and finish setup

--- interaction/mainnet.snippets.sh
+++ interaction/mainnet.snippets.sh
@@ -1,13 +1,10 @@
-ADDRESS=
+ADDRESS=erd1qqqqqqqqqqqqqpgqs5w0wfmf5gw7qae82upgu26cpk2ug8l245qszu3dxf
 PROXY=https://gateway.xoxno.com
-PROJECT="/Users/mihaieremia/GitHub/rs-liquid-xoxno/output/rs-liquid-xoxno.wasm"
+PROJECT="./output-docker/rs-liquid-xoxno/rs-liquid-xoxno.wasm"
 
 deploy() {
-    mxpy contract deploy --bytecode=${PROJECT} --recall-nonce \
-    --ledger --ledger-account-index=0 --ledger-address-index=0 \
-    --guardian erd1789rujqce0ya72k03h2jp3pgqf3vdtt0e8740tndfj0jstx3w78qxcewr8 \
-    --guardian-service-url https://tools.multiversx.com/guardian \
-    --guardian-2fa-code 730372 --version 2 --options 2 \
+    mxpy contract deploy --bytecode=${PROJECT} --arguments str:XOXNO-c1293a --recall-nonce \
+    --ledger --ledger-account-index=0 --ledger-address-index=7 \
     --gas-limit=150000000 --send --proxy=${PROXY} --chain=1 || return
 
     echo "New smart contract address: ${ADDRESS}"
@@ -15,7 +12,51 @@ deploy() {
 
 upgrade() {
     echo "Upgrade smart contract address: ${ADDRESS}"
-    mxpy contract upgrade --metadata-payable ${ADDRESS} --bytecode=${PROJECT} --recall-nonce \
-    --ledger --ledger-account-index=0 --ledger-address-index=0 \
+    mxpy  contract upgrade ${ADDRESS} --bytecode=${PROJECT} --recall-nonce \
+    --ledger --ledger-account-index=0 --ledger-address-index=7 \
+    --gas-limit=150000000 --send --proxy=${PROXY} --chain="1" || return
+}
+
+registerLsToken() {
+    mxpy contract call ${ADDRESS} --recall-nonce --function="registerLsToken" \
+    --arguments str:LXOXNO str:LXOXNO 0x12 --value 50000000000000000 \
+    --ledger --ledger-account-index=0 --ledger-address-index=7 \
     --gas-limit=150000000 --send --proxy=${PROXY} --chain=1 || return
+}
+
+registerUnstakeToken() {
+    mxpy contract call ${ADDRESS} --recall-nonce --function="registerUnstakeToken" \
+    --arguments str:UXOXNO str:UXOXNO 0x00 --value 50000000000000000 \
+    --ledger --ledger-account-index=0 --ledger-address-index=7 \
+    --gas-limit=150000000 --send --proxy=${PROXY} --chain=1 || return
+}
+
+setStateActive() {
+    mxpy contract call ${ADDRESS} --recall-nonce --function="setStateActive" \
+    --ledger --ledger-account-index=0 --ledger-address-index=7 \
+    --gas-limit=15000000 --send --proxy=${PROXY} --chain=1 || return
+}
+
+getExchangeRate() {
+    mxpy --verbose contract query ${ADDRESS} \
+        --proxy=${PROXY} \
+        --function="getExchangeRate"
+}
+
+getLsTokenAmountForMainTokenAmount() {
+    mxpy --verbose contract query ${ADDRESS} \
+        --proxy=${PROXY} \
+        --function="getLsTokenAmountForMainTokenAmount" --arguments 1000000000000000000
+}
+
+getMainTokenAmountForPosition() {
+    mxpy --verbose contract query ${ADDRESS} \
+        --proxy=${PROXY} \
+        --function="getMainTokenAmountForPosition" --arguments 892262748273425358
+}
+
+verifyContract() {
+    mxpy --verbose contract verify "${ADDRESS}"  \
+    --packaged-src=./output-docker/rs-liquid-xoxno/rs-liquid-xoxno-0.0.0.source.json --verifier-url="https://play-api.multiversx.com" \
+    --docker-image="multiversx/sdk-rust-contract-builder:v8.0.0" --ledger --ledger-account-index=0 --ledger-address-index=7  || return 
 }
